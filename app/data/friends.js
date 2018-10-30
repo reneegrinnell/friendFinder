b42bbd30c47e6ea699522cdac73698405413ebe9
Renée Grinnell <renee.grinnell@gmail.com>
2018-10-29 21:43:31 -0400

    initial setup

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+// You should save your application's data inside of `app/data/friends.js` as an array of objects. 
+// Each of these objects should roughly follow the format below.
+
+// ```json
+// {
+//   "name":"Ahmed",
+//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//   "scores":[
+//       5,
+//       1,
+//       4,
+//       4,
+//       5,
+//       1,
+//       2,
+//       5,
+//       4,
+//       1
+//     ]
+// }
---
+++ server.js
@@ -0,0 +1,1 @@
+// Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`.
