01b9a144312a3713f885e587e95e44d619e664d1
Renée Grinnell <renee.grinnell@gmail.com>
2018-11-03 18:01:41 -0400

    friend array complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,166 @@
-// You should save your application's data inside of `app/data/friends.js` as an array of objects. 
-// Each of these objects should roughly follow the format below.
+// Application data stored in an array of objects
+var friendsArray = [
+    {
+        name: "Sirius Black",
+        photo: "https://images.pottermore.com/bxd3o8b291gf/3J0jXe7qfuy6kmCkCQOsW8/7018520c6d7fda37c54186ed789d3808/SiriusBlack_WB_F5_SiriusSmilingInDoorway_HP5D-11158.jpg?w=1200",
+        scores: [
+            "1",
+            "1",
+            "5",
+            "3",
+            "5",
+            "2",
+            "1",
+            "5",
+            "1",
+            "5"
+        ]
+    },
+    {
+        name: "Neville Longbottom",
+        photo: "https://img.maximummedia.ie/joe_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtam9lLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE1XFxcLzA0XFxcLzE4MTI1NDQ0XFxcL25ldmlsbGUuanBnXCIsXCJ3aWR0aFwiOjc2NyxcImhlaWdodFwiOjQzMSxcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3LmpvZS5pZVxcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2pvZVxcXC9uby1pbWFnZS5wbmc_dj01XCJ9IiwiaGFzaCI6IjU3MjQ2NjlkY2QzY2NkMTY5M2NlOWJjNTFlZWZmZWU5MGU3YjQzMDEifQ==/neville.jpg",
+        scores: [
+            "3",
+            "1",
+            "1",
+            "1",
+            "4",
+            "1",
+            "2",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Draco Malfoy",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/16/Draco_Mal.JPG/220px-Draco_Mal.JPG",
+        scores: [
+            "1",
+            "5",
+            "4",
+            "4",
+            "5",
+            "2",
+            "5",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Severus Snape",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Ootp076.jpg/220px-Ootp076.jpg",
+        scores: [
+            "1",
+            "4",
+            "1",
+            "3",
+            "3",
+            "5",
+            "1",
+            "5",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: "Minerva McGonagall",
+        photo: "https://vignette.wikia.nocookie.net/p__/images/5/5f/McGonagall_the_Withch.jpeg/revision/latest?cb=20160322213035&path-prefix=protagonist",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "4",
+            "5",
+            "5",
+            "1",
+            "2",
+            "5",
+            "3"
+        ]
+    },
+    {
+        name: "Albus Dumbledore",
+        photo: "https://amp.thisisinsider.com/images/5a73291f46a2884b0d8b45b4-750-563.jpg",
+        scores: [
+            "3",
+            "1",
+            "1",
+            "4",
+            "5",
+            "1",
+            "1",
+            "1",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Ron Weasley",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Ron_Weasley_poster.jpg/220px-Ron_Weasley_poster.jpg",
+        scores: [
+            "1",
+            "1",
+            "3",
+            "1",
+            "5",
+            "2",
+            "4",
+            "1",
+            "3",
+            "5"
+        ]
+    },
+    {
+        name: "Hermione Granger",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Hermione_Granger_poster.jpg/220px-Hermione_Granger_poster.jpg",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "5",
+            "5",
+            "5",
+            "1",
+            "2",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Harry Potter",
+        photo: "https://www.irishtimes.com/polopoly_fs/1.3170107.1501253408!/image/image.jpg_gen/derivatives/box_620_330/image.jpg",
+        scores: [
+            "1",
+            "1",
+            "5",
+            "1",
+            "5",
+            "2",
+            "1",
+            "5",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Rubeus Hagrid",
+        photo: "https://vignette.wikia.nocookie.net/harrypotter/images/5/52/Hagrid_moleskin_overcoat.png/revision/latest?cb=20161121052755",
+        scores: [
+            "2",
+            "1",
+            "4",
+            "3",
+            "5",
+            "1",
+            "1",
+            "5",
+            "1",
+            "3"
+        ]
+    },
 
-// ```json
-// {
-//   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-//   "scores":[
-//       5,
-//       1,
-//       4,
-//       4,
-//       5,
-//       1,
-//       2,
-//       5,
-//       4,
-//       1
-//     ]
-// }
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-// Your `server.js` file should require the basic npm packages we've used in class: `express` and `path`.
+// DEPENDENCIES
+var express = require("express");
+
+//instructions said to use this too...?
+var path = require("path");
+
+// EXPRESS SERVER CONFIGURATION
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER
+app.listen(PORT, function() {
+  console.log("Wizard Friend Finder is listening on PORT: " + PORT);
+});
